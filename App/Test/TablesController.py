d77b11db8406e73977e1c7400502dc02d93d9296
Adrian Czupak <adrianczup@gmail.com>
2020-11-21 14:09:41 +0100

    start some UI integration

--- App/Test/TablesController.py
+++ App/Test/TablesController.py
@@ -2,18 +2,51 @@ from Test.InsurerController import InsurerController
 from Test.InsuranceController import InsuranceController
 from Test.LocationController import LocationController
 from Test.GlobalPositionController import GlobalPositionController
-from Test.WorkerController import WorkerController
+from Test.WorkerController import WorkerController, convert_workers_to_pd_dataframe
 from Test.GlobalWorkerPreferenceController import GlobalWorkerPreferenceController
+from database_objects.Objects import GlobalWorkerPreference, GlobalWorker, Location
+from orm_controllers.DbConnection import DbConnectionAdrianSarajevo
+import pandas as pd
+import datetime
+
 
 class TablesController():
     def __init__(self, connection):
         self.connection = connection
+        self.query = connection.session.query
         self.insurers = InsurerController(connection)
         self.locations = LocationController(connection)
         self.insurances = InsuranceController(connection)
         self.positions = GlobalPositionController(connection)
         self.workers = WorkerController(connection)
         self.preferences = GlobalWorkerPreferenceController(connection)
+    # DO OGARNIĘCIA JESZCZE
+    # def get_possible_exchanges(self):
+    #     exchange_possibilities = list()
+    #     workers = self.query(GlobalWorker).filter(
+    #         GlobalWorker.id == GlobalWorkerPreference.worker_id)
+    #     for wor in workers:
+    #         exchange_possibilities.append(self.getPropsFor(wor))
+    #     return exchange_possibilities
+    #
+    #
+    # def getPropsFor(self, worker):
+    #     workers = self.query(GlobalWorker.name,
+    #                          GlobalWorker.surname,
+    #                          Location.name).\
+    #         filter(GlobalWorker.position_id == worker.position_id).\
+    #         filter(GlobalWorker.id == GlobalWorkerPreference.worker_id).\
+    #         filter(GlobalWorkerPreference.location_id == worker.location_id).\
+    #         filter(Location.id == GlobalWorker.location_id)
+    #     workers_list = [(worker.name,
+    #                      worker.surname,
+    #                      worker.location.name,
+    #                      new_worker.worker.name,
+    #                      new_worker.worker.surname,
+    #                      new_worker.worker.location.name)
+    #                     for new_worker in workers]
+    #     return pd.DataFrame(workers_list, columns=['name1', 'surname1', 'location1',
+    #                                                'name2', 'surname2', 'location2'])
 
     # def getAllGlobalInsurances(self):
     #     print(self.session.query(GlobalInsurance.id, GlobalInsurance.expiration_date, Insurer.name).
@@ -33,14 +66,14 @@ class TablesController():
     #                              GlobalWorkerPreference.worker.surname,
     #                              GlobalWorkerPreference.location.name))
     #
-    # def getPropsFor(self, worker):
-    #     print(self.session.query(GlobalWorkerPreference.worker.name,
-    #                              GlobalWorkerPreference.worker.surname,
-    #                              GlobalWorkerPreference.location.name).
-    #           filter(GlobalWorkerPreference.worker.position_id == worker.position_id).
-    #           filter(GlobalWorkerPreference.location_id == worker.location_id))
 
 
-# if __name__ == '__main__':
-#     testInstance = TablesController()
-#     testInstance.getAllWorkers()
+if __name__ == '__main__':
+    testInstance = TablesController(DbConnectionAdrianSarajevo())
+    # print(testInstance.getPropsFor(GlobalWorker(id=126,
+    #                                             name='Cindy',
+    #                                             surname='Bennet',
+    #                                             position_id=4,
+    #                                             location_id=2
+    #                                             )))
+    # print(testInstance.get_possible_exchanges())
--- App/ui/main_win.py
+++ App/ui/main_win.py
@@ -1,5 +1,8 @@
 from ui.main_window import Ui_MainWindow
-
+from orm_controllers.DbConnection import DbConnectionAdrianSarajevo
+from Test.TablesController import TablesController
+from Test.WorkerController import convert_workers_to_pd_dataframe
+from Test.GlobalWorkerPreferenceController import convert_preferences_to_pd_dataframe
 
 class MainWindow(Ui_MainWindow):
     def __init__(self):
@@ -7,13 +10,22 @@ class MainWindow(Ui_MainWindow):
 
     def setupUi(self, mainWindow):
         super().setupUi(mainWindow)
+        self.controller = TablesController(DbConnectionAdrianSarajevo())
         self.__connectSlots()
 
     def __connectSlots(self):
         self.show_all_workers_btn.clicked.connect(self.__show_all_workers_btn_clicked)
+        self.show_all_preferences_btn.clicked.connect(self.__show_all_preferences_btn_clicked)
+        self.show_local_preferences_btn.clicked.connect(self.__sho_possible_exchange_btn_clicked) #change button name
 
     def __show_all_workers_btn_clicked(self):
-        pass
+        self.textEdit.setText(convert_workers_to_pd_dataframe(self.controller.workers.get_all()).to_string())
+
+    def __show_all_preferences_btn_clicked(self):
+        self.textEdit.setText(convert_preferences_to_pd_dataframe(self.controller.preferences.get_all()).to_string())
+
+    def __sho_possible_exchange_btn_clicked(self):
+        self.textEdit.setText(convert_preferences_to_pd_dataframe(self.controller.get_possible_exchanges()).to_string())
 
 
 
