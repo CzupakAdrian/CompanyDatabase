11475690e2a19e27c2b9f1aabf6f39ed0b0e8c0b
PapaSmerf <albert.lis@interia.pl>
2020-11-21 20:14:45 +0100

    Add some slots implementation and new dialogs

--- App/ui/main_win.py
+++ App/ui/main_win.py
@@ -1,3 +1,5 @@
+from PyQt5 import QtWidgets
+
 from Test.TablesController import TablesController
 from ui.main_window import Ui_MainWindow
 
@@ -25,6 +27,9 @@ class MainWindow(Ui_MainWindow):
         self.show_insurances_btn.clicked.connect(self.__show_insurances_btn_clicked)
         self.delete_insurance_btn.clicked.connect(self.__delete_insurance_btn_clicked)
         self.add_insurance_btn.clicked.connect(self.__add_insurance_btn_clicked)
+        self.show_insurers_btn.clicked.connect(self.__show_insurers_btn_clicked)
+        self.add_insurer_btn.clicked.connect(self.__add_insurer_btn_clicked)
+        self.delete_insurer_btn.clicked.connect(self.__delete_insurer_btn_clicked)
         self.show_positions_btn.clicked.connect(self.__show_positions_btn_clicked)
         self.add_position_btn.clicked.connect(self.__add_position_btn_clicked)
         self.delete_position_btn.clicked.connect(self.__delete_position_btn_clicked)
@@ -41,13 +46,37 @@ class MainWindow(Ui_MainWindow):
         pass
 
     def __show_worker_btn_clicked(self):
-        pass
+        self.textEdit.clear()
+        dialog = QtWidgets.QDialog()
+        from ui.sh_worker import ShowWorkerDialog
+        ui = ShowWorkerDialog()
+        ui.setupUi(dialog)
+        dialog.show()
+        id = dialog.exec_()
+        if id > 0:
+            try:
+                worker = self.__tc.workers.get_worker(id)
+                self.textEdit.append(f'Name: {worker.name}, Surname:{worker.surname}, Location: {worker.location.name}, '
+                                     f'Position: {worker.position.position}, Insurance id: {worker.insurance_id}, '
+                                     f'Birth_date: {worker.birth_date}')
+            except:
+                self.textEdit.append('Wrong id')
 
     def __add_worker_btn_clicked(self):
         pass
 
     def __delete_worker_btn_clicked(self):
-        pass
+        dialog = QtWidgets.QDialog()
+        from ui.del_worker import DelWorkerDialog
+        ui = DelWorkerDialog()
+        ui.setupUi(dialog)
+        dialog.show()
+        id = dialog.exec_()
+        if id > 0:
+            try:
+                self.__tc.workers.delete_worker(id)
+            except:
+                self.textEdit.append('Wrong id')
 
     def __show_all_preferences_btn_clicked(self):
         pass
@@ -56,7 +85,22 @@ class MainWindow(Ui_MainWindow):
         pass
 
     def __show_worker_preference_btn_clicked(self):
-        pass
+        self.textEdit.clear()
+        dialog = QtWidgets.QDialog()
+        from ui.sh_worker import ShowWorkerDialog
+        ui = ShowWorkerDialog()
+        ui.setupUi(dialog)
+        dialog.show()
+        id = dialog.exec_()
+        if id > 0:
+            try:
+                preference = self.__tc.preferences.get_preference(id)
+                self.textEdit.append(
+                    f'Name: {preference.worker.name}, Surname:{preference.worker.surname}, Current location: '
+                    f'{preference.worker.location.name}, '
+                    f'Position: {preference.worker.position.position}, Desired location: {preference.location.name}')
+            except:
+                self.textEdit.append('Wrong id')
 
     def __delete_preference_btn_clicked(self):
         pass
@@ -75,6 +119,19 @@ class MainWindow(Ui_MainWindow):
         pass
 
     def __add_insurance_btn_clicked(self):
+        pass
+
+    def __show_insurers_btn_clicked(self):
+        self.textEdit.clear()
+        insurers = self.__tc.insurers.get_all()
+        from Test.InsurerController import convert_insurers_to_pd_dataframe
+        insurers = convert_insurers_to_pd_dataframe(insurers).to_string()
+        self.textEdit.append(insurers)
+
+    def __add_insurer_btn_clicked(self):
+        pass
+
+    def __delete_insurer_btn_clicked(self):
         pass
 
     def __show_positions_btn_clicked(self):
